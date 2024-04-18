cfadc525190c90858c0667a2c4ffebcc00ed3244
Carles Loriente <7197733+carlesloriente@users.noreply.github.com>
2024-04-18 17:26:14 +0200

    Applied stylelint rules

--- stylelint.config.js
+++
@@ -1,9 +0,0 @@
-// stylelint.config.js
-import { defineConfig } from 'stylelint-define-config';
-
-export default defineConfig({
-  extends: '@archoleat/stylelint-config-extended-scss',
-  rules: {
-    'declaration-empty-line-before': null,
-  },
-});
